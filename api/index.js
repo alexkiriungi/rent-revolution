1d1e8dd244dd39097189f461ccb4f09124214bec
alexkiriungi <mwasxander@gmail.com>
2024-02-23 02:15:29 +0300

    Database connection to mongoDB complete

--- api/index.js
+++ api/index.js
@@ -1,7 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+
+dotenv.config();
+const port = process.env.PORT || 3001;
 const app = express();
-
-app.listen(3000, () => {
-    console.log('Server is connected at port: ')
+mongoose.connect(process.env.MONGO_URL).then(() => {
+    console.log('Database connection successful');
 })
+.catch((error) => {
+    console.log(error);
+});
+
+app.listen(port, () => {
+    console.log(`Server is connected at port: ${port}`)
+});
