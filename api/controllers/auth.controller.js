eb2a67f1e450021f8b5ac12c2452b18c27c89f06
alexkiriungi <mwasxander@gmail.com>
2024-02-25 00:14:01 +0300

    minor security additions to the error handling

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,8 +1,12 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from "./utils/error.js";
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
+    if (!username || !email || !password || username === '' || email === '' || password === '') {
+        next(errorHandler(400, 'All fields are required'));
+    }
     const hashedPassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({ username, email, password: hashedPassword });
     try {
