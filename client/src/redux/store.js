c59ebbddcc3948170f0b0087ea86d090122e118c
alexkiriungi <mwasxander@gmail.com>
2024-02-25 17:14:00 +0300

    Added redux-toolkit

---
+++ client/src/redux/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import userReducer from './user/userSlice';
+
+export const store = configureStore({
+    reducer: { user: userReducer },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+        serializableCheck: false,
+    }),
+});
