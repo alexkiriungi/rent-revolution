a4b33e2fea41b002ba23810f7d34caf32717d744
alexkiriungi <mwasxander@gmail.com>
2024-02-26 20:00:17 +0300

    created the user Upadte API

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,27 @@
-export const updateUser = async (req, res, next) => {
+import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
+import bcryptjs from 'bcryptjs';
 
+export const updateUser = async (req, res, next) => {
+    if (req.user.id !== req.params.id) {
+        return next(errorHandler(401, "you can only update your account"));
+    }
+    try {
+        if (req.body.password) {
+            req.body.password = bcryptjs.hashSync(req.body.password, 10);
+        }
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+            $set: {
+                username: req.body.username,
+                email: req.body.email,
+                password: req.body.password,
+                avatar: req.body.avatar,
+            }
+        }, { new: true });
+
+        const { password, ...rest } = updatedUser._doc;
+        res.status(200).json(rest);
+    } catch (error) {
+        next(error);
+    }
 };
