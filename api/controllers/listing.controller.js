879e895592b9e8fd166d949a6b8e41108ac18d46
alexkiriungi <mwasxander@gmail.com>
2024-02-28 11:20:43 +0300

    Added the delete listing API route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 import Listing from '../models/listing.model.js';
+import { errorHandler } from '../utils/error.js';
 
 export const createListing = async (req, res, next) => {
     try {
@@ -7,4 +8,21 @@ export const createListing = async (req, res, next) => {
     } catch (error) {
         next(error);
     }
+};
+
+export const deleteListing = async (req, res, next) => {
+     const listing = Listing.findById(req.params.id);
+     if (!listing) {
+        return next(errorHandler(401, 'Listing not found!'));
+     }
+     if (req.user.id !== listing.userRef) {
+        return next(errorHandler(401, 'You can only delete your own listing!'));
+     }
+     try {
+        await Listing.findByIdAndDelete(req.params.id);
+        res.sttaus(200).json('Listing has been deleted!')
+     } catch (error) {
+        next(error);
+     }
+
 };
