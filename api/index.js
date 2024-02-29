55c303d5358e93068f8175080b9a0495ae9e911f
alexkiriungi <mwasxander@gmail.com>
2024-02-29 04:09:35 +0300

    Edited the index.js file for deployment prep

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
 import listingRoutes from './routes/listing.route.js';
+import path from 'path';
 
 
 dotenv.config();
@@ -17,13 +18,17 @@ mongoose.connect(process.env.MONGO_URL).then(() => {
 .catch((error) => {
     console.log(error);
 });
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/listing', listingRoutes);
 
-
+app.use(express.static(path.join(__dirname, '/client/dist')));
+app.get('*', (req, res) => {
+    res.sendFid(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 app.listen(port, () => {
     console.log(`Server is connected at port: ${port}`)
