a9699dd4d8e354b4f3a53dc785121314ba27b060
alexkiriungi <mwasxander@gmail.com>
2024-02-29 04:03:05 +0300

    Added the About page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,34 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>
+        About Rent Revolution
+      </h1>
+      <p className='mb-4 text-slate-700' >
+        Welcome to our cutting-edge real estate website, 
+        your ultimate partner in buying, selling, or renting 
+        properties in 2024. Our platform is designed with your 
+        needs in mind, offering a seamless and engaging experience 
+        that sets us apart from the competition. Discover a wide range
+        of properties, handpicked by our team of experienced real estate
+        professionals. We specialize in connecting you with the perfect
+        homes, apartments, or studios, tailored to your 
+        unique preferences, requirements and lifestyle.
+      </p>
+      <p className='mb-4 text-slate-700' >
+        Our website boasts user-friendly search tools, enabling you to
+        quickly filter properties by type, price, amenities, and features. 
+        Stay informed with up-to-date recent section, and benefit from 
+        our recommendations throughout your real estate journey.
+      </p>
+      <p className='mb-4 text-slate-700' >
+      Don't miss out on exclusive listings and essential real estate 
+      resources – get started today by creating a free account. We're here to ensure your 
+      experience is smooth and successful, making your real estate dreams 
+      a reality in 2024 and beyond.
+      </p>
+
+    </div>
   )
 }
